e88adb90f0f9fd0650edc24a265f54a4e10df1ad
Eusébio Alves <eusebio.a95@hotmail.com>
2024-05-23 20:47:20 +0100

    Static obstacles

---
+++ scripts/base_scripts/static_obstacle.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rotation_degrees = randf_range(0, 360)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/map_square.gd
+++ scripts/map_square.gd
@@ -1,15 +1,28 @@
 class_name MapSquare extends Node2D
 
+@export var beach_things_array : Array [PackedScene] 
+
 @onready var screen_status_notifier : VisibleOnScreenNotifier2D = $ScreenStatusNotifier
 @onready var new_chunk_pos : Marker2D = $NewChunkPos
 @onready var sprite_2d : Sprite2D = $Sprite2D
+@onready var positions : Node2D = $Positions
 
+var positions_array : Array
 var chunk_holder : Node
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	positions_array = positions.get_children()
 	chunk_holder = get_tree().get_first_node_in_group("chunks")
 	load_random_sprite()
+	
+	for position in positions_array:
+		var beach_thing = beach_things_array.pick_random().instantiate()
+		beach_thing.position.x += randf_range(-5 , 5)
+		beach_thing.position.y += randf_range(-5 , 5)
+		position.add_child(beach_thing)
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
