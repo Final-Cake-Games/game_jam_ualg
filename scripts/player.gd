96b1d0bf75a1b562ea328de2c2b151da7ee49e34
Eusébio Alves <eusebio.a95@hotmail.com>
2024-05-23 14:55:20 +0100

    Player scene with basic 8dir movement

---
+++ scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var max_speed : float = 500
+@export var accel : float = 50
+@export var decel : float = 10
+
+var current_speed : float
+
+
+func _physics_process(delta):
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if input_direction:
+		current_speed = move_toward(current_speed, max_speed, accel)
+		velocity = input_direction * current_speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, decel)
+	
+	move_and_slide()
