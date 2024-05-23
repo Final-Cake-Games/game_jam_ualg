44c3dec08841c39672b4fe65a2f6d0b0535f7333
Eusébio Alves <eusebio.a95@hotmail.com>
2024-05-23 13:36:48 +0100

    Base game scene
    Camera movement currently constant and chunk holder node to keep current active chunk.

---
+++ scripts/game.gd
@@ -0,0 +1,21 @@
+class_name Game extends Node2D
+
+@export var chunk : MapSquare
+@export var camera_move_speed : float = 25
+
+@onready var camera_2d = $Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	move_camera(delta)
+	
+func move_camera(delta : float) -> void:
+	camera_2d.position.y -= camera_move_speed * delta
